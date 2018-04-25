c010d0f9a86909c802079858cf5b0f52a469da5b
leemaster <leemast3r@outlook.com>
2018-04-25 23:03:08 +0800

    init the categories service

---
+++ src/test/java/cn/leemaster/flowers/categories/CategoriesApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.leemaster.flowers.categories;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CategoriesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
