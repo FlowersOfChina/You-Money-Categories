f4fc46ff21fb216e635acff0bffae3e57a093fe5
leemaster <leemast3r@outlook.com>
2018-04-26 15:10:12 +0800

    update

--- src/main/java/cn/leemaster/flowers/categories/CategoriesApplication.java
+++ src/main/java/cn/leemaster/flowers/categories/CategoriesApplication.java
@@ -2,8 +2,10 @@ package cn.leemaster.flowers.categories;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CategoriesApplication {
 
 	public static void main(String[] args) {
